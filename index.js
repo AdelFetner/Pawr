0725c0d3cdfd8c8d396ce241f64a7a5e03aaebd0
Adel Fetner <adelensoc@gmail.com>
2022-02-28 19:51:09 -0300

    Modificado .js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-document.getElementById("redirect-btn").onclick = function (){
-    open("#")
-}
 document.getElementById("logo").onclick = function (){
     open("#")
 }
@@ -19,14 +16,6 @@ window.onscroll = function () {
 }
 
 
-// redirección del cuarto artículo
-
-document.querySelector("#container-fourth-article").onclick = function (){
-    open("#")
-}
-
-
-
 // abrir y cerrar login
 
 // variables 
@@ -35,27 +24,71 @@ let openLogIn = document.querySelector("#login")
 
 let openRegister = document.querySelector("#register")
 
-let loginPopUp = document.querySelector("#login-back")
+let logInPopUp = document.querySelector("#login-back")
+
+let registerPopUp = document.querySelector("#register-back")
+
+let btnMainRegister = document.querySelector("#redirect-btn")
+
+let fourthArticle = document.querySelector("#container-fourth-article")
+
+let registerRedirect = document.querySelector("#redirect-register-popup")
+
+let logInRedirect = document.querySelector("#redirect-login-popup")
 
 // abrir
 
-openLogIn.onclick = function(){
-    loginPopUp.style.display = "flex"
+openLogIn.onclick = function abrirLogIn(){
+    logInPopUp.style.display = "flex"
 }
 
-openRegister.onclick = function(){
-    loginPopUp.style.display = "flex"
+openRegister.onclick = function abrirRegister(){
+    registerPopUp.style.display = "flex"
+}
+
+btnMainRegister.onclick = function abrirRegister(){
+    registerPopUp.style.display = "flex"
+}
+
+fourthArticle.onclick = function abrirRegister(){
+    registerPopUp.style.display = "flex"
+}
+
+registerRedirect.onclick = function abrirRegister(){
+    registerPopUp.style.display = "flex"
+    logInPopUp.style.display = "none"
+}
+
+logInRedirect.onclick = function abrirLogIn(){
+    logInPopUp.style.display = "flex"
+    registerPopUp.style.display = "none"
 }
 
 // cerrar
 
 
-loginPopUp.onclick = function (){
-    loginPopUp.style.display = "none";
+// loginPopUp.onclick = function (){
+//     loginPopUp.style.display = "none";
+// }
+
+logInPopUp.onclick = function (evento) {
+    if (evento.target === logInPopUp) {
+    logInPopUp.style.display = "none";
+    }
 }
 
+registerPopUp.onclick = function (evento) {
+    if (evento.target === registerPopUp) {
+    registerPopUp.style.display = "none";
+    }
+}
 
-// redirección login
+// redirección login y register
 document.querySelector("#login-logo").onclick = function (){
     open("#")
 }
+document.querySelector("#register-logo").onclick = function (){
+    open("#")
+}
+
+
